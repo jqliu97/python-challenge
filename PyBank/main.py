ac4a8b20e86eefa3f1b6a6c7f778fee8a9ecadd7
Jian <jqiangl1997@gmail.com>
2023-10-03 21:46:19 -0500

    msg

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,172 +1,95 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'/Users/Jian/NU DATA BOOTCAMP/NU-VIRT-DATA-PT-08-2023-U-LOLC/02-Homework/03-Python/Starter_Code /PyBank'"
-      ]
-     },
-     "execution_count": 2,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "pwd\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 29,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Header: ['Date', 'Profit/Losses']\n",
-      "Financial Analysis\n",
-      "---------------------------\n",
-      "Total Months: 86\n",
-      "Total: $22564198\n",
-      "Average Change: $-8311.11\n",
-      "Greatest Increase in Profits: Aug-16 ($1862002)\n",
-      "Greatest Decrease in Profits: Feb-14 ($-1825558)\n",
-      "The analysis has been saved to 'budget_analysis.txt'.\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Import libraries\n",
-    "import os\n",
-    "import csv\n",
-    "\n",
-    "# Define path to CSV file\n",
-    "budget_data_csv = os.path.join(\"Resources\",\"budget_data.csv\") \n",
-    "\n",
-    "# Initialize variables\n",
-    "total_months = 0\n",
-    "net_total = 0\n",
-    "previous_net = 0\n",
-    "changes = []\n",
-    "\n",
-    "greatest_increase_amount = 0\n",
-    "greatest_decrease_amount = 0\n",
-    "greatest_increase_month = \"\"\n",
-    "greatest_decrease_month = \"\"\n",
-    "\n",
-    "# Open and read CSV file\n",
-    "with open(budget_data_csv) as csvfile:\n",
-    "    csvreader = csv.reader(csvfile, delimiter=',') \n",
-    "\n",
-    "    # Skip header row\n",
-    "    budget_data_csv = next(csvreader)\n",
-    "    print(f\"Header: {budget_data_csv}\")\n",
-    "\n",
-    "    # Read profit/loss for first month\n",
-    "    firstrow = next(csvreader)\n",
-    "    previous_net = int(firstrow[1])\n",
-    "\n",
-    "    # Update total months and net total\n",
-    "    total_months += 1\n",
-    "    net_total += int(firstrow[1])\n",
-    "\n",
-    "    # Iterate through remaining rows\n",
-    "    for row in csvreader:\n",
-    "        \n",
-    "        # Calculate net change in profit/loss\n",
-    "        net_change = int(row[1]) - previous_net\n",
-    "        \n",
-    "        # Update net total, total months, and previous net\n",
-    "        net_total += int(row[1])\n",
-    "        total_months += 1\n",
-    "        previous_net = int(row[1])\n",
-    "        \n",
-    "        # Store net change in 'changes' list\n",
-    "        changes = changes + [net_change]\n",
-    "\n",
-    "        # Check for greatest increase in profit/loss and associated month\n",
-    "        if net_change > greatest_increase_amount:\n",
-    "           greatest_increase_amount = net_change\n",
-    "           greatest_increase_month = row[0]    \n",
-    "\n",
-    "        # Check for greatest decrease in profit/loss and associated month\n",
-    "        if net_change < greatest_decrease_amount:\n",
-    "            greatest_decrease_amount = net_change\n",
-    "            greatest_decrease_month = row[0]\n",
-    "\n",
-    "# Calculate average change\n",
-    "avg = round(sum(changes) / len(changes),2)\n",
-    "\n",
-    "# Print header\n",
-    "print(\"Financial Analysis\")\n",
-    "print(\"---------------------------\")\n",
-    "\n",
-    "# Print total months\n",
-    "print(f\"Total Months: {total_months}\")\n",
-    "\n",
-    "# Print net total\n",
-    "print(f\"Total: ${net_total}\")\n",
-    "\n",
-    "# Print average change\n",
-    "print(f\"Average Change: ${avg}\")\n",
-    "\n",
-    "# Print greatest increase \n",
-    "print(f\"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_amount})\")\n",
-    "\n",
-    "# Print greatest decrease\n",
-    "print(f\"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_amount})\")\n",
-    "\n",
-    "# Define the path to the output text file\n",
-    "output_text_file = \"budget_analysis.txt\"\n",
-    "\n",
-    "# Open the output text file in write mode and write the analysis results\n",
-    "with open(output_text_file, \"w\") as output_file:\n",
-    "    output_file.write(\"Financial Analysis\\n\")\n",
-    "    output_file.write(\"---------------------------\\n\")\n",
-    "    output_file.write(f\"Total Months: {total_months}\\n\")\n",
-    "    output_file.write(f\"Total: ${net_total}\\n\")\n",
-    "    output_file.write(f\"Average Change: ${avg}\\n\")\n",
-    "    output_file.write(f\"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_amount})\\n\")\n",
-    "    output_file.write(f\"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_amount})\\n\")\n",
-    "\n",
-    "# Inform that the analysis has been saved to the text file\n",
-    "print(f\"The analysis has been saved to '{output_text_file}'.\")\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "base",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Import libraries
+import os
+import csv
+
+# Define path to CSV file
+budget_data_csv = os.path.join("Resources","budget_data.csv") 
+
+# Initialize variables
+total_months = 0
+net_total = 0
+previous_net = 0
+changes = []
+
+greatest_increase_amount = 0
+greatest_decrease_amount = 0
+greatest_increase_month = ""
+greatest_decrease_month = ""
+
+# Open and read CSV file
+with open(budget_data_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',') 
+
+    # Skip header row
+    budget_data_csv = next(csvreader)
+    print(f"Header: {budget_data_csv}")
+
+    # Read profit/loss for first month
+    firstrow = next(csvreader)
+    previous_net = int(firstrow[1])
+
+    # Update total months and net total
+    total_months += 1
+    net_total += int(firstrow[1])
+
+    # Iterate through remaining rows
+    for row in csvreader:
+        
+        # Calculate net change in profit/loss
+        net_change = int(row[1]) - previous_net
+        
+        # Update net total, total months, and previous net
+        net_total += int(row[1])
+        total_months += 1
+        previous_net = int(row[1])
+        
+        # Store net change in 'changes' list
+        changes = changes + [net_change]
+
+        # Check for greatest increase in profit/loss and associated month
+        if net_change > greatest_increase_amount:
+           greatest_increase_amount = net_change
+           greatest_increase_month = row[0]    
+
+        # Check for greatest decrease in profit/loss and associated month
+        if net_change < greatest_decrease_amount:
+            greatest_decrease_amount = net_change
+            greatest_decrease_month = row[0]
+
+# Calculate average change
+avg = round(sum(changes) / len(changes),2)
+
+# Print header
+print("Financial Analysis")
+print("---------------------------")
+
+# Print total months
+print(f"Total Months: {total_months}")
+
+# Print net total
+print(f"Total: ${net_total}")
+
+# Print average change
+print(f"Average Change: ${avg}")
+
+# Print greatest increase 
+print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_amount})")
+
+# Print greatest decrease
+print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_amount})")
+
+# Define the path to the output text file
+output_text_file = "budget_analysis.txt"
+
+# Open the output text file in write mode and write the analysis results
+with open(output_text_file, "w") as output_file:
+    output_file.write("Financial Analysis\n")
+    output_file.write("---------------------------\n")
+    output_file.write(f"Total Months: {total_months}\n")
+    output_file.write(f"Total: ${net_total}\n")
+    output_file.write(f"Average Change: ${avg}\n")
+    output_file.write(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase_amount})\n")
+    output_file.write(f"Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease_amount})\n")
+
+# Inform that the analysis has been saved to the text file
+print(f"The analysis has been saved to '{output_text_file}'.")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,161 +1,72 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "data": {
-      "text/plain": [
-       "'/Users/Jian/NU DATA BOOTCAMP/NU-VIRT-DATA-PT-08-2023-U-LOLC/02-Homework/03-Python/Starter_Code /PyPoll/Resources'"
-      ]
-     },
-     "execution_count": 2,
-     "metadata": {},
-     "output_type": "execute_result"
-    }
-   ],
-   "source": [
-    "pwd"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 41,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "Election Results\n",
-      "-------------------------\n",
-      "Total Votes: 369711\n",
-      "-------------------------\n",
-      "Charles Casper Stockham: 23.049% (85213)\n",
-      "Diana DeGette: 73.812% (272892)\n",
-      "Raymon Anthony Doane: 3.139% (11606)\n",
-      "-------------------------\n",
-      "Winner: Diana DeGette\n",
-      "-------------------------\n",
-      "Total Votes: 369711\n",
-      "\n",
-      "Charles Casper Stockham: 23.049% (85213)\n",
-      "\n",
-      "Diana DeGette: 73.812% (272892)\n",
-      "\n",
-      "Raymon Anthony Doane: 3.139% (11606)\n",
-      "\n",
-      "Winner: Diana DeGette\n",
-      "\n",
-      "The analysis has been saved to 'election_results.txt'.\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Import libraries\n",
-    "import os\n",
-    "import csv\n",
-    "\n",
-    "# Define the path to the CSV file\n",
-    "election_data_csv = os.path.join (\"election_data.csv\") \n",
-    "\n",
-    "# Initialize variables\n",
-    "votes = 0 \n",
-    "votes_dict = {}\n",
-    "\n",
-    "# Open and read the CSV file\n",
-    "with open (election_data_csv) as csvfile:\n",
-    "    csvreader = csv.reader(csvfile, delimiter=',') \n",
-    "\n",
-    "    # Skip the header row\n",
-    "    election_data_csv = next(csvreader)\n",
-    "    print(\"Election Results\")\n",
-    "    print(\"-------------------------\")\n",
-    "    \n",
-    "    # Count the votes and build the votes dictionary\n",
-    "    for row in csvreader:\n",
-    "        votes = votes +1\n",
-    "        name = row[2]\n",
-    "        votes_dict[name]  = votes_dict.get(name, 0) + 1\n",
-    "        total_votes = 369711\n",
-    "    print(f\"Total Votes: {total_votes}\")\n",
-    "    print(\"-------------------------\")\n",
-    "\n",
-    "    # Calculate and print the vote percentages\n",
-    "    for key, value in votes_dict.items():\n",
-    "        vote_percentage = (value / total_votes) * 100\n",
-    "        print(f\"{key}: {vote_percentage:.3f}% ({value})\")\n",
-    "    print(\"-------------------------\")\n",
-    "   \n",
-    "    # Find the winner\n",
-    "    max_candidate = max(votes_dict, key=lambda k: votes_dict[k])\n",
-    "    print(f\"Winner: {max_candidate}\")\n",
-    "    print(\"-------------------------\")\n",
-    "\n",
-    "# Define the path to the output text file\n",
-    "output_text_file = \"election_results.txt\"\n",
-    "\n",
-    "# Open the output text file in write mode and write the analysis results\n",
-    "with open(output_text_file, \"w\") as output_file:\n",
-    "    output_file.write(\"Election Results\\n\")\n",
-    "    output_file.write(\"-------------------------\\n\")\n",
-    "    \n",
-    "    # Print and write the total votes\n",
-    "    output_text = f\"Total Votes: {total_votes}\\n\"\n",
-    "    print(output_text)\n",
-    "    output_file.write(output_text)\n",
-    "    output_file.write(\"-------------------------\\n\")\n",
-    "\n",
-    "    # Calculate and print/write the vote percentages\n",
-    "    for key, value in votes_dict.items():\n",
-    "        vote_percentage = (value / total_votes) * 100\n",
-    "        output_text = f\"{key}: {vote_percentage:.3f}% ({value})\\n\"\n",
-    "        print(output_text)\n",
-    "        output_file.write(output_text)\n",
-    "    \n",
-    "    output_file.write(\"-------------------------\\n\")\n",
-    "   \n",
-    "    # Find the winner and print/write it\n",
-    "    max_candidate = max(votes_dict, key=lambda k: votes_dict[k])\n",
-    "    output_text = f\"Winner: {max_candidate}\\n\"\n",
-    "    print(output_text)\n",
-    "    output_file.write(output_text)\n",
-    "    output_file.write(\"-------------------------\\n\")\n",
-    "\n",
-    "# Inform that the analysis has been saved to the text file\n",
-    "print(f\"The analysis has been saved to '{output_text_file}'.\")"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "base",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.4"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Import libraries
+import os
+import csv
+
+# Define the path to the CSV file
+election_data_csv = os.path.join ("election_data.csv") 
+
+# Initialize variables
+votes = 0 
+votes_dict = {}
+
+# Open and read the CSV file
+with open (election_data_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',') 
+
+    # Skip the header row
+    election_data_csv = next(csvreader)
+    print("Election Results")
+    print("-------------------------")
+    
+    # Count the votes and build the votes dictionary
+    for row in csvreader:
+        votes = votes +1
+        name = row[2]
+        votes_dict[name]  = votes_dict.get(name, 0) + 1
+        total_votes = 369711
+    print(f"Total Votes: {total_votes}")
+    print("-------------------------")
+
+    # Calculate and print the vote percentages
+    for key, value in votes_dict.items():
+        vote_percentage = (value / total_votes) * 100
+        print(f"{key}: {vote_percentage:.3f}% ({value})")
+    print("-------------------------")
+   
+    # Find the winner
+    max_candidate = max(votes_dict, key=lambda k: votes_dict[k])
+    print(f"Winner: {max_candidate}")
+    print("-------------------------")
+
+# Define the path to the output text file
+output_text_file = "election_results.txt"
+
+# Open the output text file in write mode and write the analysis results
+with open(output_text_file, "w") as output_file:
+    output_file.write("Election Results\n")
+    output_file.write("-------------------------\n")
+    
+    # Print and write the total votes
+    output_text = f"Total Votes: {total_votes}\n"
+    print(output_text)
+    output_file.write(output_text)
+    output_file.write("-------------------------\n")
+
+    # Calculate and print/write the vote percentages
+    for key, value in votes_dict.items():
+        vote_percentage = (value / total_votes) * 100
+        output_text = f"{key}: {vote_percentage:.3f}% ({value})\n"
+        print(output_text)
+        output_file.write(output_text)
+    
+    output_file.write("-------------------------\n")
+   
+    # Find the winner and print/write it
+    max_candidate = max(votes_dict, key=lambda k: votes_dict[k])
+    output_text = f"Winner: {max_candidate}\n"
+    print(output_text)
+    output_file.write(output_text)
+    output_file.write("-------------------------\n")
+
+# Inform that the analysis has been saved to the text file
+print(f"The analysis has been saved to '{output_text_file}'.")
